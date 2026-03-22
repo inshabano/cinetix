43bc20e7667c83714a6b43ba1861f14db6bcf271
Insha <108580895+inshabano@users.noreply.github.com>
2026-03-22 20:24:45 +0530

    updated backend url

--- client/booking-app/src/services/axiosinstance.js
+++ client/booking-app/src/services/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { message } from 'antd';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://api.inshabano.live';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://cinetixbackend.onrender.com';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
